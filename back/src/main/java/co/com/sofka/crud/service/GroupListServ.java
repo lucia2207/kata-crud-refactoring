53e4d30e299bbad14f3fc71f944756d912178732
lucia2207 <ludef15@gmail.com>
2022-02-25 20:51:43 -0300

    Si se borra un grupo se borran las tareas asociadas

--- back/src/main/java/co/com/sofka/crud/service/GroupListServ.java
+++ back/src/main/java/co/com/sofka/crud/service/GroupListServ.java
@@ -1,15 +1,22 @@
 package co.com.sofka.crud.service;
 
 import co.com.sofka.crud.model.GroupList;
+import co.com.sofka.crud.model.Todo;
 import co.com.sofka.crud.repository.GroupListRepo;
+import co.com.sofka.crud.repository.TodoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class GroupListServ {
 
     @Autowired
     private GroupListRepo repository;
+
+    @Autowired
+    private TodoRepository todoRepository;
 
     public Iterable<GroupList> list(){
         return repository.findAll();
@@ -20,6 +27,15 @@ public class GroupListServ {
     }
 
     public void delete(Long id){
+        List<Todo> aborrar = (List<Todo>) todoRepository.findByGroupListId(id);
+
+        // si no esta vacio el grupo borrar los todos
+        if (!aborrar.isEmpty()){
+            aborrar.forEach(todo -> {
+                todoRepository.delete(todo);
+            });
+        }
+
         repository.delete(get(id));
     }
 
